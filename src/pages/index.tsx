ff1419bd4e0e99a7e9967abed7066422dc107376
imaimai17468 <tosiki_801_life@outlook.jp>
2023-10-15 20:13:22 +0900

    feat: エディタのデザインを修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,10 @@ const Editor = dynamic(() => import("./components/Editor"), { ssr: false });
 
 export default function App() {
   return (
-    <div>
-      <Editor />
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <div className="w-1/2 border border-gray-300 p-3 h-4/5 rounded-xl bg-white shadow-lg shadow-white overflow-y-scroll">
+        <Editor />
+      </div>
     </div>
   );
 }
