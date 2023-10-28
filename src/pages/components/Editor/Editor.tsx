6beb7fd926d74ed9974c758f6dbdb9aca4e41a3e
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-10-28 15:42:09 +0900

    move what isn't page component file to other than pages dir

--- src/pages/components/Editor/Editor.tsx
+++ src/pages/components/Editor/Editor.tsx
@@ -1,8 +1,8 @@
 "use client"; // this registers <Editor> as a Client Component
+import { getRandomUser } from "@/utils/randomUser";
 import { BlockNoteEditor } from "@blocknote/core";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
-import { getRandomUser } from "@/pages/utils/randomUser";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import * as Y from "yjs";
 const doc = new Y.Doc();
 
@@ -10,7 +10,7 @@ const { WebsocketProvider } = require("y-websocket");
 const provider = new WebsocketProvider("ws://localhost:1234", "draw-room", doc);
 
 export default function Editor() {
-  const editor: BlockNoteEditor | null = useBlockNote({
+  const editor: BlockNoteEditor = useBlockNote({
     collaboration: {
       provider,
       fragment: doc.getXmlFragment("document-store"),
